ecb9a8c36c789aa1ef9266056e59264b5546a2d9
ajcrosbie <aaron.j.crosbie@gmail.com>
2022-09-24 15:43:22 +0100

    added message formating functionalised the code

--- uvReader.py
+++ uvReader.py
@@ -1,20 +1,22 @@
 import time
+import writeEmail
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 
-driver = webdriver.Chrome("chromedriver.exe")
-driver.get("https://weather.com/en-MT/weather/hourbyhour/l/Huddersfield+England+United+Kingdom?canonicalCityId=9d2d451b1f11fcbd3a2cd65d86022f5113788c93903af32b8c77845f8c598d10")
-# clear cookie pop up
-time.sleep(5)
-cookies = driver.find_element(by=By.ID, value="truste-consent-required")
-cookies.click()
-time.sleep(5)
-# 
-driver.fullscreen_window()
+
+
+def setup():
+    driver = webdriver.Chrome("chromedriver.exe")
+    driver.get("https://weather.com/en-MT/weather/hourbyhour/l/Huddersfield+England+United+Kingdom?canonicalCityId=9d2d451b1f11fcbd3a2cd65d86022f5113788c93903af32b8c77845f8c598d10")
+    # clear cookie pop up
+    time.sleep(5)
+    cookies = driver.find_element(by=By.ID, value="truste-consent-required")
+    cookies.click()
+    time.sleep(5)
+    # 
+    driver.fullscreen_window()
+    return driver
+
 # make the driver keep trying to click if it's not working as intended
 def clickRecurson(current, count):
     count = count+1
@@ -26,10 +28,10 @@ def clickRecurson(current, count):
         time.sleep(2)
         clickRecurson(current, count)
 
-HOURS = 24
-uvs = []
 # looks HOURS hours ahead
 def findUvs(driver):
+    HOURS = 24
+    uvs = []
     for i in range(HOURS):
         current = driver.find_element(by=By.ID, value="detailIndex" + str(i))
         if current.text[1] == ":":
@@ -46,8 +48,28 @@ def findUvs(driver):
             uv = uvHalf[1:2]
         uvs.append((uv, currentHour))
         # time.sleep(5)
+    return uvs
 
+def findDanger(uvs):
+    dangers = []
+    for i in range(len(uvs)):
+        if int(uvs[i][0]) >=3:
+            dangers.append(uvs[i])
+    return dangers
 
-time.sleep(4)
-driver.quit()
-print(len(uvs))
+def formatString(dangers):
+    string = ""
+    for i in dangers:
+        string = string + "sun indx " + i[0] + " at "+ i[1]+" "
+        string = string + "\n"
+    return string
+
+def main():
+    driver = setup()
+    uvs = findUvs(driver)
+    dangers = findDanger(uvs)
+    print(formatString(dangers))
+    
+    time.sleep(4)
+    driver.quit()
+main()
